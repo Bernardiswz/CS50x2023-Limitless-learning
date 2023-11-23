7066590c4a94abaa8635955943d9974fc46c461b
Bernardiswz <alekhinebernardo@gmail.com>
2023-11-22 21:08:56 -0300

    more progress as usual

--- app/static/js/flashcards/flashcards.js
+++ app/static/js/flashcards/flashcards.js
@@ -1,42 +1,48 @@
 function main() {
     var flashcards = document.querySelectorAll(".buttons-container .list-group-item");
-    var flashcardsDiv = document.getElementById("flashcards"); // Div of the flashcards
-    const flashcardPage = document.getElementById("flashcard-page"); // Dynamism on the same page for the flashcards functionality
+    var flashcardsDiv = document.getElementById("flashcards");
+    const flashcardPage = document.getElementById("flashcard-page");
     const flashcardForm = document.getElementById("flashcard-form");
     var flashcardAnswer;
 
-    // Elements of current flashcard div
+    const flashcardFeedback = document.getElementById("flashcard-feedback");
+    const feedbackUserAnswer = document.getElementById("user-answer");
+    const feedbackFlashcardAnswer = document.getElementById("flashcard-answer");
+
     const flashcardTopic = document.getElementById("topic");
     const flashcardQuestion = document.getElementById("question");
-
-    var isFlashcardPage = false;
-
-    console.log("Flashcards:", flashcards.length);
 
     flashcards.forEach(function (button) {
         button.addEventListener("click", function (event) {
             event.preventDefault();
 
-            var topic = button.querySelector("p") ? button.querySelector("p").textContent: "Topic not found";
-            var question = button.querySelector("h4") ? button.querySelector("h4").textContent : "Question not found";
-            var answer = button.querySelector(".answer") ? button.querySelector(".answer").textContent : "Answer not found";
+            var topic = button.querySelector("p.mb-1") ? button.querySelector("p.mb-1").textContent : "Topic not found";
+            var question = button.querySelector("h4.mb-1") ? button.querySelector("h4.mb-1").textContent : "Question not found";
+            var answer = button.parentElement.querySelector(".answer") ? button.parentElement.querySelector(".answer").textContent : "Answer not found";
             flashcardAnswer = answer;
+
+            console.log(flashcardAnswer);
 
             flashcardTopic.textContent = topic;
             flashcardQuestion.textContent = question;
-            
+
             flashcardsDiv.style.display = "none";
             flashcardPage.style.display = "block";
-            isFlashcardPage = true;
         });
     });
 
-    if (isFlashcardPage) {
-        flashcardForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-            const answer = flashcardForm.answer.value;
-        });
-    }
+    flashcardForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formAnswer = flashcardForm.answer.value;
+
+        flashcardPage.style.display = "none";
+
+        feedbackUserAnswer.textContent = formAnswer;
+        feedbackFlashcardAnswer.textContent = flashcardAnswer;
+
+        flashcardFeedback.style.display = "block";
+    });
 }
 
 document.addEventListener("DOMContentLoaded", main);
